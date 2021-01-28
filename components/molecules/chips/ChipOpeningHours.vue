fa8ab78eb4b4c9c9edf556d264d1440d8773752d
marcos vinicius <marcvinicius.1997@gmail.com>
2021-01-28 19:02:52 -0300

    feat: chip para exibicao do status do lugar

---
+++ components/molecules/chips/ChipOpeningHours.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  name: 'ChipOpeningHours',
+
+  props: {
+    place: {
+      type: Object,
+      default: () => {}
+    }
+  }
+}
+</script>
+
+<template lang="pug">
+v-chip(
+  small
+  text-color="white"
+  v-if="place.opening_hours"
+  :color="place.opening_hours.open_now ? 'green' : 'red'"
+)
+  div(v-if="place.opening_hours.open_now")
+    | Aberto
+  div(v-else)
+    | Fechado
+
+</template>
