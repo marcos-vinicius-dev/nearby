c1ded5ef4028ef277c93467e9b7fdd2431a67f1d
marcos vinicius <marcvinicius.1997@gmail.com>
2021-01-28 19:02:20 -0300

    feat: botao sair

---
+++ components/molecules/buttons/BtnLogount.vue
@@ -0,0 +1,27 @@
+<script>
+const Cookie = process.client ? require('js-cookie') : undefined
+
+export default {
+  name: 'BtnLogount',
+
+  methods: {
+    __logout () {
+      Cookie.remove('auth')
+      this.$store.commit('user/setAuth', null)
+      this.$router.push('/')
+    }
+  }
+
+}
+</script>
+
+<template lang="pug">
+v-btn(
+  block
+  dark
+  depressed
+  @click="__logout"
+)
+  | Sair
+
+</template>
