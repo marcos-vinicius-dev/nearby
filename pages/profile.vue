d3631f8bf3b979160719e775b2896dc793b44020
marcos vinicius <marcvinicius.1997@gmail.com>
2021-01-28 19:09:44 -0300

    feat: pagina de alteracao dos dados do usuario

---
+++ pages/profile.vue
@@ -0,0 +1,39 @@
+<script>
+import FormUpdateProfile from '@/components/organisms/forms/FormUpdateProfile.vue'
+
+import { ValidationObserver, ValidationProvider } from 'vee-validate'
+
+export default {
+  name: 'PageProfile',
+
+  components: {
+    FormUpdateProfile,
+    ValidationObserver,
+    ValidationProvider
+  },
+
+  middleware: 'authenticated'
+}
+</script>
+
+<template lang="pug">
+v-row(
+  class="full-height"
+  justify="center"
+  align="center"
+)
+  v-col(
+    cols="12"
+    sm="8"
+    md="8"
+    lg="4"
+  )
+    v-card(class="pa-5 shadow-card")
+      v-col(cols="10")
+        h1(class="text-h5 font-weight-bold ")
+          | Perfil
+
+      v-col(cols="12")
+        form-update-profile
+
+</template>
