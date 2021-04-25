948a00fe59d2b9f2e93001a07d7a684fbfa29d62
Marcos Vinicius <marcos.silva@athonenergia.com.br>
2021-04-25 02:27:52 -0300

    remove color

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,13 +1,5 @@
-import colors from 'vuetify/es5/util/colors'
-
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
-
-  server: {
-    host: '0.0.0.0',
-    port: 3000
-  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -86,25 +78,6 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     proxy: true
-  },
-
-  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
-  vuetify: {
-    customVariables: ['~/assets/css/variables.scss'],
-    theme: {
-      dark: false,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
